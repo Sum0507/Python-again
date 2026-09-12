2b07ce7e476ec39f37f761892031bbd081104f01
Jannatun Nur Sumaiya <jsumaiya290@gmail.com>
2026-09-13 00:49:04 +0600

    Add files via upload

---
+++ 1stCode.py
@@ -0,0 +1,34 @@
+print("Hello World")
+print("First code for the 100th time.")
+print(5+3)
+
+Name="Ms. Nur"
+Age=20
+print(Name)
+print(Age)
+
+print("My name is",Name,"& my age is",Age,"something")
+print(type(Name))
+print(type(Age))
+
+#Arithmetic operator
+a=5
+b=7
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+#assignment operator
+sum=1
+print("sum :", sum)
+sum+=1
+print("sum :", sum)
+
+Name=input("Enter ur name : ")
+print("Name : ", Name)
+
+X= int(input("Enter value of X :"))
+Y=  int(input("Enter value of Y :"))
+print("sum : ", X + Y)
---
+++ 2ndcode.py
@@ -0,0 +1,22 @@
+# str="Hola me allamo Sumaiya. \nY tu?"
+# len1=len(str)
+# print("len = ", len1)
+# print(str)
+# ch1=str[0]
+# print(ch1)
+# print(str[0:7])
+
+# print(str.replace("Sumaiya", "Sumu"))
+# Name=input("Enter your name: ")
+# print(len(Name))
+# print(Name.count("a"))
+
+# n=int(input("enter num : "))
+# if n%2==0 :
+#     print("even")
+# elif n%2!=0 :
+#     print("Odd")
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
