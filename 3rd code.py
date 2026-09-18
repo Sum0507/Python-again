ea52d60564805ff2b899ac02dccb39df33ddab64
Jannatun Nur Sumaiya <jsumaiya290@gmail.com>
2026-09-19 04:49:12 +0600

    Add files via upload

---
+++ 3rd code.py
@@ -0,0 +1,29 @@
+# Num=[1, 1.1, 2.2, 3.33, 4.4, 5]
+# print(Num)
+# print(Num[3])
+# print(Num[1:4])
+
+# List=[ "A", "X"]
+# print(List)
+# List.reverse()
+# print(List)
+
+# Tup=(1,2,3)
+# tup1=(1,)
+# print(Tup)
+# print(tup1)
+# Name=("S", "U", "M","A" ,"I" ,"Y" ,"A")
+# print(Name.count("A"))
+
+#prac
+list=[1,2,3,2,4]
+print(list)
+
+# list1=list.copy()
+# list1.reverse()
+# print(list1)
+
+# if list == list1:
+#     print("yes")
+# elif list!=list1:
+#     print("no")    
